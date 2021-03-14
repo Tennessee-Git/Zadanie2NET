f7918728c2a9dab78d7d605684edc8c83dc07776
Tennessee <awramiukofficial@gmail.com>
2021-03-14 17:03:57 +0100

    Add project files.

---
+++ Zadanie2.net/Forms/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zadanie2.net.Forms
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60, MinimumLength = 3), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string Street { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
+
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Miasto")]
+
+        public string City { get; set; }
+
+        [Display(Name = "Numer")]
+        public int Number { get; set; }
+
+    }
+}
---
+++ Zadanie2.net/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Zadanie2.net.Forms;
+
+namespace Zadanie2.net.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        { }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+                return Page();
+            else
+                return RedirectToPage("./Privacy");
+        }
+    }
+}
